5a5d328fb53f703640bcf395a2927845b6f4fa56
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-26 22:05:30 +0530

    Reverse_The_Number_3

--- Assignment/Codes-Java/ReverseNo.java
+++ Assignment/Codes-Java/ReverseNo.java
@@ -1,19 +1,50 @@
+import java.util.Scanner;
+
 /**
  * ReverseNo
  */
 public class ReverseNo {
     public static void main(String[] args) {
-        int num = 7853;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num: ");
+        int num = sc.nextInt();
+        int Reverse = rev(num);
+        System.out.println(Reverse);
+    }
+
+    static int rev(int num){
         int var = 0;
-        int temp = num;
         while(num>0){
-            int rem = num % 10;
-            var = var*10 + rem;   //var = var*10 + (num%10) //without taking extra variable rem
+            var = var*10 + num%10;
             num/=10;
         }
-        System.out.println("reverse no. = " + var);
+        return var;
     }
+    
 }
+
+
+
+
+
+
+
+// /**
+//  * ReverseNo
+//  */
+// public class ReverseNo {
+//     public static void main(String[] args) {
+//         int num = 7853;
+//         int var = 0;
+//         int temp = num;
+//         while(num>0){
+//             int rem = num % 10;
+//             var = var*10 + rem;   //var = var*10 + (num%10) //without taking extra variable rem
+//             num/=10;
+//         }
+//         System.out.println("reverse no. = " + var);
+//     }
+// }
 
 
 
