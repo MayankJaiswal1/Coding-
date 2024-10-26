44c4c5c512f0ed0930819a07e948cf29c8d4da67
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-26 21:58:49 +0530

    Reverse_The_Number_2

--- Assignment/Codes-Java/ReverseNo.java
+++ Assignment/Codes-Java/ReverseNo.java
@@ -1,16 +1,50 @@
+/**
+ * ReverseNo
+ */
 public class ReverseNo {
     public static void main(String[] args) {
-        int num = 320;
-        System.out.println(rev(num));
-    }
-
-    static int rev(int num ){
-        int reverse = 0;
+        int num = 7853;
+        int var = 0;
+        int temp = num;
         while(num>0){
-            reverse = reverse*10 + (num%10);
-            num/=10; 
+            int rem = num % 10;
+            var = var*10 + rem;   //var = var*10 + (num%10) //without taking extra variable rem
+            num/=10;
         }
-        return reverse;
+        System.out.println("reverse no. = " + var);
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class ReverseNo {
+//     public static void main(String[] args) {
+//         int num = 320;
+//         System.out.println(rev(num));
+//     }
+
+//     static int rev(int num ){
+//         int reverse = 0;       // reverse = var
+//         while(num>0){
+//             reverse = reverse*10 + (num%10);
+//             num/=10; 
+//         }
+//         return reverse;
+//     }
+    
+// }
