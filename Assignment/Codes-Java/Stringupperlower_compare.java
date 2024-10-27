b62954ee9ccf8ee2c9d7281ddd813cea4dd573a1
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-27 21:56:27 +0530

    String_Upper_Lower_compare&convert

--- Assignment/Codes-Java/Stringupperlower_compare.java
+++ Assignment/Codes-Java/Stringupperlower_compare.java
@@ -1,24 +1,52 @@
 // count no. of upper and lower case letter and which one more convert string to that
 
+// public class Stringupperlower_compare {
+//     public static void main(String[] args) {
+//         String str = "AsdfGTYreXEWR";
+//         System.out.println(convertStr(str));
+//     }
+
+//     static String convertStr(String str){
+//         int uppercount = 0, Lowercount =0;
+//         for(char c : str.toCharArray()){
+//             if(Character.isUpperCase(c))
+//                 uppercount++;
+//             else
+//                 Lowercount++;    
+//         }
+//         if(uppercount>Lowercount){
+//             return str.toUpperCase();
+//         }else{
+//             return str.toLowerCase();
+//         } 
+//     }
+    
+// }
+
+
+/**
+ * Stringupperlower_compare
+ */
 public class Stringupperlower_compare {
+
     public static void main(String[] args) {
-        String str = "AsdfGTYreXEWR";
-        System.out.println(convertStr(str));
+        String str = "Hifhy ASDFdeRTFED ";
+        System.out.println(convert(str));
     }
 
-    static String convertStr(String str){
-        int uppercount = 0, Lowercount =0;
+    static String convert(String str){
+        int upperCount = 0; int lowerCount = 0;
         for(char c : str.toCharArray()){
-            if(Character.isUpperCase(c))
-                uppercount++;
-            else
-                Lowercount++;    
+            if(Character.isUpperCase(c)){
+                upperCount++;
+            }else{
+                lowerCount++;
+            }
         }
-        if(uppercount>Lowercount){
+        if(upperCount > lowerCount){
             return str.toUpperCase();
         }else{
             return str.toLowerCase();
-        } 
+        }
     }
-    
 }
