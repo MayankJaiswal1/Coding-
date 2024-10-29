7968d1132c571dbd139af08bb5237ad21d61fb35
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-29 21:16:26 +0530

    Count_Digit_Sum_II

--- Assignment/Codes-Java/Count_digit_sum.java
+++ Assignment/Codes-Java/Count_digit_sum.java
@@ -12,6 +12,32 @@
 // }
 
 
+// class Count_digit_sum{
+//     public static void main(String[] args) {
+//         int num = 320;
+//         int rem = 0;
+//         int sum = 0;
+//         while(num>0){
+//             rem = num%10;
+//             num/=10;
+//             sum+=rem;
+//         }
+//         System.out.println(sum);
+//         int temp = sum;
+//         int count = 0;
+//         for(int i = 1; i<=sum; i++){
+//             if(sum % i == 0){
+//                 count++;
+//             }
+//         }
+//         if(count == 2){
+//             System.out.println("Googly");
+//         }
+//     }
+// }
+
+
+
 class Count_digit_sum{
     public static void main(String[] args) {
         int num = 320;
@@ -25,12 +51,12 @@ class Count_digit_sum{
         System.out.println(sum);
         int temp = sum;
         int count = 0;
-        for(int i = 1; i<=sum; i++){
-        //while(sum > 0){
-            if(sum % i == 0){
+        //for(int i = 1; i<=sum; i++){
+        while(temp > 0){
+            if(sum % temp == 0){
                 count++;
             }
-            //temp --; 
+            temp --; 
         }
         if(count == 2){
             System.out.println("Googly");
