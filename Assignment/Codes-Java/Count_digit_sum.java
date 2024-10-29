d92ddf1560b2cd8ce539fc0faeabdad3d77ce7cd
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-29 21:14:57 +0530

    Count_Digit_Sum

--- Assignment/Codes-Java/Count_digit_sum.java
+++ Assignment/Codes-Java/Count_digit_sum.java
@@ -1,12 +1,39 @@
-public class Count_digit_sum {
+// public class Count_digit_sum {
+//     public static void main(String[] args) {
+//         int num = 97531;
+//         int sum = 0;
+//         while(num>0){
+//             int rem = num%10;
+//             num/=10;
+//             sum+=rem;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+
+class Count_digit_sum{
     public static void main(String[] args) {
-        int num = 97531;
+        int num = 320;
+        int rem = 0;
         int sum = 0;
         while(num>0){
-            int rem = num%10;
+            rem = num%10;
             num/=10;
             sum+=rem;
         }
         System.out.println(sum);
+        int temp = sum;
+        int count = 0;
+        for(int i = 1; i<=sum; i++){
+        //while(sum > 0){
+            if(sum % i == 0){
+                count++;
+            }
+            //temp --; 
+        }
+        if(count == 2){
+            System.out.println("Googly");
+        }
     }
 }
