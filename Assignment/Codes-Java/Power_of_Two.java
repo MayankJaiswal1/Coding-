7fbdc3fb0066f77fce7b35c9a570395cd05d5b2d
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-10-31 23:00:02 +0530

    is_Power_Two_pow(n)-II

--- Assignment/Codes-Java/Power_of_Two.java
+++ Assignment/Codes-Java/Power_of_Two.java
@@ -5,6 +5,9 @@ public class Power_of_Two {
         }else{
             System.out.println("no");
         }
+
+        System.out.println(isPowerOfTwo_2(16) ? "Yes" : "No");
+        //System.out.println(isPowerOfTwo_3(64) ? "Yes" : "No");
     }
 
     static boolean isPowerOfTwo(int n){
@@ -13,4 +16,20 @@ public class Power_of_Two {
         }
         return (int)(Math.ceil((Math.log(n) / Math.log(2)))) == (int)(Math.floor(Math.log(n) / Math.log(2)));
     }
+
+    static boolean isPowerOfTwo_2(int n){
+        if( n == 0){
+            return false;
+        }
+
+        while(n !=1 ){
+            if(n%2 != 0){
+                return false;
+            }
+            n/=2;
+        }
+        return true;
+    }
+
+    
 }
