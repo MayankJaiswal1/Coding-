8fcab201693b8f05687f521c9e981d506430452e
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-03 21:42:23 +0530

    Word_count_String-II

--- Assignment/Codes-Java/String/word_count.java
+++ Assignment/Codes-Java/String/word_count.java
@@ -1,3 +1,43 @@
+// package String;
+
+// public class word_count {
+//     public static void main(String[] args) {
+//         String str = "Hi ISR is a field of IR callesd ISR";
+//         System.out.println("No. of words: " + countwords(str));
+//     }
+    
+//     static int countwords(String str){
+//         if(str.length() == 0){
+//             return 0;
+//         }
+//         int count = 0;
+//         int state = 0;
+
+//         for(int i=0; i<str.length(); i++){
+//             if(str.charAt(i) == '\\'){
+//                 i++;
+//                 continue;
+//             }
+//             if(Character.isLetterOrDigit(str.charAt(i))){
+//                 if(state == 0){
+//                     count++;
+//                     state = 1;
+//                 }
+//             }
+//             else{
+//                 state = 0;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+
+
+
 package String;
 
 public class word_count {
@@ -7,27 +47,10 @@ public class word_count {
     }
     
     static int countwords(String str){
-        if(str.length() == 0){
+        if ( str == null || str.isEmpty())
             return 0;
-        }
-        int count = 0;
-        int state = 0;
-
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) == '\\'){
-                i++;
-                continue;
-            }
-            if(Character.isLetterOrDigit(str.charAt(i))){
-                if(state == 0){
-                    count++;
-                    state = 1;
-                }
-            }
-            else{
-                state = 0;
-            }
-        }
-        return count;
+        
+    String[] words = str.split("\\s+");  
+    return words.length;
     }
 }
