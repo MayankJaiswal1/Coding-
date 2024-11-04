9ca38550362e2e457244f7892dd5784dc3b8250d
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-04 21:34:49 +0530

    Cyclically_Rotate_Array_I

---
+++ Assignment/Codes-Java/Array/Cyclically_Rotate_array.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Cyclically_Rotate_array {
+
+    static int arr[] = new int[] {1, 2, 3, 4, 5};
+    public static void main(String[] args) {
+        System.out.println("Given Array is : ");
+        System.out.println(Arrays.toString(arr));
+        rotate_I();
+        System.out.println("Rotated Array is : ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void rotate_I(){
+        int last_el = arr[arr.length - 1];
+        int i;
+        for(i = arr.length -1; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = last_el;
+    }
+
+}
