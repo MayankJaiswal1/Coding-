896dfa42d1f0456416e3d1104ff8b1813eee4ab2
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-09 21:19:21 +0530

    Sort_Positive_Negative_in_an_Array_II

--- Assignment/Codes-Java/Array/positive_negative_element_move.java
+++ Assignment/Codes-Java/Array/positive_negative_element_move.java
@@ -13,9 +13,34 @@ public class positive_negative_element_move {
         for(int e : arr){
             System.out.print(e + " ");
         }
+
+        int arr1[] = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+        int n = arr1.length;
+        rearrange(arr1, n);
+        printArray(arr1, n);
     }
 
     static void move(int arr[]){
         Arrays.sort(arr);
+    }
+
+    static void rearrange(int arr[], int n){
+        int j = 0, temp;
+        for(int i=0; i<n; i++){
+            if(arr[i]<0){
+                if(i!=j){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
+    }
+
+    static void printArray(int arr[], int n){
+        for(int i = 0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
