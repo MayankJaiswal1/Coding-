abeb5b87417aa9a2ffd572ac64f3ca79993e9885
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-12 20:51:35 +0530

    Sort_Wave_array_in_an_Array_II

---
+++ Assignment/Codes-Java/Array/Wave_array.java
@@ -0,0 +1,44 @@
+/*Input:  arr[] = {10, 5, 6, 3, 2, 20, 100, 80}
+Output: arr[] = {10, 5, 6, 2, 20, 3, 100, 80} 
+Explanation: 
+here you can see {10, 5, 6, 2, 20, 3, 100, 80} first element is larger than the second and the same thing is repeated again and again. large element – small element-large element -small element and so on .it can be small element-larger element – small element-large element -small element too. all you need to maintain is the up-down fashion which represents a wave. there can be multiple answers.
+
+
+Input: arr[] = {20, 10, 8, 6, 4, 2}
+Output: arr[] = {20, 8, 10, 4, 6, 2} */
+
+package Array;
+
+import java.util.Arrays;
+
+public class Wave_array {
+    public static void main(String[] args) {
+        int arr[] = {10,90,49,2,1,5,23,52,45};
+        int n = arr.length;
+        Wave_array ob = new Wave_array();
+        ob.sortWave(arr, n);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+
+    
+    // This function sorts arr[0..n-1] in wave form, i.e.,
+    // arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4]..
+    void sortWave(int arr[], int n){
+        // Sort the input array
+        Arrays.sort(arr);
+        // Swap adjacent elements
+        for(int i=0; i<n-1; i+=2){
+            swap(arr, i, i+1);
+        }
+    }
+
+     // A utility method to swap two numbers.
+    void swap(int arr[], int a , int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    
+}
