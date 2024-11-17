b491d5a8b3b689674546d9671e417f33a9e68c22
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-17 21:56:43 +0530

    Find_Missing_Integer_in_an_Array_I

---
+++ Assignment/Codes-Java/Array/Find_Missing_Integer.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class Find_Missing_Integer {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,5};
+        int n = 5;
+        System.out.println(missingNumber(n,arr));
+    }
+
+    static int missingNumber(int n, int arr[]){
+        int hash[] = new int[n+1];
+        for(int i=0; i<n-1; i++){
+            hash[arr[i]]++;
+        }
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
