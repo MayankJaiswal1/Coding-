2462fe7da8674e9d81164c067c7b64d5fac54f9d
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-11-30 11:44:29 +0530

    Triplet_sum_in_Array_I

---
+++ Assignment/Codes-Java/Array/Triplet_sum_in_array.java
@@ -0,0 +1,42 @@
+/*
+ * Input: arr = {12, 3, 4, 1, 6, 9}, sum = 24; 
+Output: 12, 3, 9 
+Explanation: There is a triplet (12, 3 and 9) present 
+in the array whose sum is 24. 
+
+
+Input: arr = {1, 2, 3, 4, 5}, sum = 9 
+Output: 5, 3, 1 
+Explanation: There is a triplet (5, 3 and 1) present 
+in the array whose sum is 9.
+
+
+Input: arr = {2, 10, 12, 4, 8}, sum = 9 
+Output: No Triplet
+Explanation: We do not print in this case and return false.
+ */
+
+package Array;
+
+public class Triplet_sum_in_array {
+    public static void main(String[] args) {
+        int arr[] = {1,4,45,6,10,8};
+        int sum = 22;
+        find3Numbers(arr, sum);
+    }
+
+    static boolean find3Numbers(int arr[], int sum){
+        int n = arr.length;
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                for(int k=j+1; k<n; k++){
+                    if(arr[i] + arr[j] + arr[k] == sum){
+                        System.out.println("Triplet is " + arr[i] +"," +  arr[j] + ","+ arr[k]);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
