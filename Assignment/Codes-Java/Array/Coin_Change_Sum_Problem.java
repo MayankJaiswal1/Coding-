810962655cd29b1a5b5ae9fa6a46cac6140dea7d
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-12 21:47:25 +0530

    Coin_Sum_Problem_Array_II

--- Assignment/Codes-Java/Array/Coin_Change_Sum_Problem.java
+++ Assignment/Codes-Java/Array/Coin_Change_Sum_Problem.java
@@ -40,8 +40,13 @@ public class Coin_Change_Sum_Problem {
     }
 
     static int countRecur(int coins[],int n, int sum){
+         // If sum is 0 then there is 1 solution
+        // (do not include any coin)
         if(sum == 0) return 1;
+        // 0 ways in the following two cases
         if(sum<0 || n==0) return 0;
+        // count is sum of solutions (i)
+        // including coins[n-1] (ii) excluding coins[n-1]
         return countRecur(coins, n, sum-coins[n-1]) + countRecur(coins, n-1, sum);
     }
 }
