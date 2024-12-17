acba035b605065b0c0c6cf2013e379222610da13
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-17 22:09:55 +0530

    Positive_Negative_Num_I

---
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Positive_Negative_No.java
@@ -0,0 +1,14 @@
+package T0P_50_PrepInsta;
+
+public class Positive_Negative_No {
+    public static void main(String[] args) {
+        int num = 5;
+        if(num > 0){
+            System.out.println("The number is Positive ");
+        }else if(num < 0){
+            System.out.println("The number is Negative ");
+        }else{
+            System.out.println("Zero");
+        }
+    }
+}
