ffe5b059b7628fd4629386dc1dd0e60429e5dd02
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-18 17:52:24 +0530

    Even_Odd_Num_I

---
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Even_ODD_no.java
@@ -0,0 +1,12 @@
+package T0P_50_PrepInsta;
+
+public class Even_ODD_no {
+    public static void main(String[] args) {
+        int num = 29;
+        if(num % 2 == 0){
+            System.out.println(num+ "is even");
+        }else{
+            System.out.println(num + " is odd");
+        }
+    }
+}
