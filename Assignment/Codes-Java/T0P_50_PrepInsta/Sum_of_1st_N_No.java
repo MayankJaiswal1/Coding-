82dcce09159812230370726d25f05a5973ba3730
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-19 20:25:59 +0530

    Sum_of_1st_N_No_I

---
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Sum_of_1st_N_No.java
@@ -0,0 +1,12 @@
+package T0P_50_PrepInsta;
+
+public class Sum_of_1st_N_No {
+    public static void main(String[] args) {
+        int num = 10;
+        int sum = 0;
+        for(int i=1; i<=num; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
