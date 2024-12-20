50b998f563bf5112fa3dc9ef927e0d35c41a4980
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-20 19:14:50 +0530

    Sum_of_No._in_range_I

---
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Sum_Of_No_in_range.java
@@ -0,0 +1,13 @@
+package T0P_50_PrepInsta;
+
+public class Sum_Of_No_in_range {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int sum = 0;
+        for(int i=a; i<=b; i++){
+            sum += i;
+        }
+        System.out.println("The sum is : " + sum );
+    }
+}
