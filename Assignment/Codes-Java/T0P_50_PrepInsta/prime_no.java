6069707d0423fc628e5020797ec1e8b8c2492ae5
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-22 13:13:50 +0530

    Leap_Year_I

---
+++ Assignment/Codes-Java/T0P_50_PrepInsta/prime_no.java
@@ -0,0 +1,17 @@
+package T0P_50_PrepInsta;
+
+public class prime_no {
+    public static void main(String[] args) {
+        int num = 11;
+        int count = 0;
+        for(int i=1; i<=num; i++){
+            if(num % i == 0){
+                count ++;
+            }
+        }
+        if(count == 2){
+            System.out.println(num + " is prime ");
+        }
+        
+    }
+}
