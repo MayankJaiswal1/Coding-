5dd36070c626de9eb156996ad54187427b8f4153
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-12-24 20:04:53 +0530

    Sum_Of_Digit_of_number_No._II

--- Assignment/Codes-Java/T0P_50_PrepInsta/Sum_Of_Digit_of_a_Number.java
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Sum_Of_Digit_of_a_Number.java
@@ -1,3 +1,10 @@
+/*Find the Sum of the Digits of a Number in Java
+Given an integer input number, the objective is t0 Find the Sum of the Digits of a Number in Java Language. To do we’ll break the number into it’s digits. Then we add them one by one as we extract them from the number using modulo operator “%”.
+
+Example
+Input : 1234
+Output : 1 + 2 + 3 + 4 = 10 */
+
 package T0P_50_PrepInsta;
 
 public class Sum_Of_Digit_of_a_Number {
