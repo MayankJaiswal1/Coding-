cd06cc0d277d077d37e8542116aee6a9ff4c2c7b
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2024-12-25 22:02:41 +0530

    Code3

---
+++ Assignment/Codes-Java/Codes/p4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class PlayerJer{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+		int jerNo = 0;
+                for(int i =1; i<=5; i++){
+                        System.out.print("Enter jersey no. for player ");
+                        jerNo = sc.nextInt();
+			System.out.println("JerNo : " + jerNo);
+                }
+
+
+                /*System.out.println("JerNo: " + jerNo1);
+                System.out.println("JerNo: " + jerNo2);
+                System.out.println("JerNo: " + jerNo3);
+                System.out.println("JerNo: " + jerNo4);
+                System.out.println("JerNo: " + jerNo4);*/
+        }
+}
