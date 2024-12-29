fa0789f941641a3b832609a940234f7e6c92f5a8
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2024-12-29 22:16:19 +0530

    4

--- Assignment/Codes-Java/Codes/p1.java
+++ Assignment/Codes-Java/Codes/p1.java
@@ -1,15 +1,5 @@
-class PlayerJer {
+class ArrayDemo{
 	public static void main(String[] args){
-		int jerNo1 = 10;
-		int jerNo2 = 7;
-		int jerNo3 = 18;
-		int jerNo4 = 12;
-		int jerNo5 = 45;
-
-		System.out.println("JerNo: " + jerNo1);
-		System.out.println("JerNo: " + jerNo2);
-		System.out.println("JerNo: " + jerNo3);		
-		System.out.println("JerNo: " + jerNo4);
-		System.out.println("JerNo: " + jerNo4);
+		int arr[] = new int[5]{};
 	}
 }
