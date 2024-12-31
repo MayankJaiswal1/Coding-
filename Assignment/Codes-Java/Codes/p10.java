84d17f2a48b5978934dd75487edfc20ddf48026b
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2024-12-31 15:29:54 +0530

    10

--- Assignment/Codes-Java/Codes/p10.java
+++ Assignment/Codes-Java/Codes/p10.java
@@ -1,17 +1,5 @@
-
-import java.util.*;
-
-class OneDArray{
+class TwoDArray{
 	public static void main(String[] args){
-		int [] empId = new int[3];
-		System.out.println(empId[0]); 
-		System.out.println(empId[1]);     
-		System.out.println(empId[2]);
-		empId[0] =10;
-		empId[1] =20;
-		empId[2] =30;
-		System.out.println(empId[0]);
-		System.out.println(empId[1]);
-		System.out.println(empId[2]);
+		int arr1[][] = new int[][];
 	}
 }
