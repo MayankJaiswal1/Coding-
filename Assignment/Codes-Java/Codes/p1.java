b75637d76d6077cc6ae09ceecb01aad95b8fa464
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-04 21:15:26 +0530

    1

--- Assignment/Codes-Java/Codes/p1.java
+++ Assignment/Codes-Java/Codes/p1.java
@@ -1,5 +1,10 @@
-class ArrayDemo{
+class JVMInternal{
 	public static void main(String[] args){
-		int arr[] = new int[5]{};
+		int x = 10; 
+		int y = 10;
+		int z = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
 	}
 }
