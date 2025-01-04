c415980b67b1c2aa23580354d4e81aae65afe430
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-04 21:16:09 +0530

    4

--- Assignment/Codes-Java/Codes/p4.java
+++ Assignment/Codes-Java/Codes/p4.java
@@ -1,20 +1,17 @@
-import java.util.*;
 
-class PlayerJer{
-        public static void main(String[] args){
-                Scanner sc = new Scanner(System.in);
-		int jerNo = 0;
-                for(int i =1; i<=5; i++){
-                        System.out.print("Enter jersey no. for player ");
-                        jerNo = sc.nextInt();
-			System.out.println("JerNo : " + jerNo);
-                }
-
-
-                /*System.out.println("JerNo: " + jerNo1);
-                System.out.println("JerNo: " + jerNo2);
-                System.out.println("JerNo: " + jerNo3);
-                System.out.println("JerNo: " + jerNo4);
-                System.out.println("JerNo: " + jerNo4);*/
-        }
+class JVMInternal{
+	public static void main(String[] args){
+		char ch1 = 'A';  //65
+		char ch2 = 'a';  //97  
+		char ch3 = 'B';  //66
+		int x = 65;
+		int y = 97;
+		int z = 66;
+		System.out.println(System.identityHashCode(ch1));
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(ch2));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(ch3));
+		System.out.println(System.identityHashCode(z));
+	}
 }
