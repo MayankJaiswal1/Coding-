5225856bfdb9cefea9e20d978bfcd27b8f5d8215
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-12 21:06:36 +0530

    3

--- Assignment/Codes-Java/Codes/p3.java
+++ Assignment/Codes-Java/Codes/p3.java
@@ -1,15 +1,20 @@
-
-class JVMInternal{
+class CharDemo{
 	public static void main(String[] args){
-		int x = 100; 
-		int y = 110;
-		int z = 100;
-		int a = 128;
-		int b = 128;
-		System.out.println(System.identityHashCode(x));
-		System.out.println(System.identityHashCode(y));
-		System.out.println(System.identityHashCode(z));
-		System.out.println(System.identityHashCode(a));
-		System.out.println(System.identityHashCode(b));
+		int arr1[] = new int[]{10,20,30,40,50};
+		System.out.println(arr1);
+		
+		float arr2[] = new float[]{10,20,30,40,50};
+		System.out.println(arr2);
+
+		byte arr3[] = new byte[]{11,22,33,44,55};
+		System.out.println(arr3);
+		
+		char arr4[] = new char[]{'A','B','C','D','E'};
+		System.out.println(arr4);
+
 	}
 }
+
+/* 
+ Here we have proof that String is internally Character Array
+ */
