f3779cae81a4691f9316e15d93ecda753153fde7
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-15 22:03:11 +0530

    13

--- Assignment/Codes-Java/Codes/p13.java
+++ Assignment/Codes-Java/Codes/p13.java
@@ -1,23 +1,11 @@
-class StringDemo{
+
+import java.util.*;
+
+class InputDemo{
 	public static void main(String[] args){
-		String str1 = "Core2Web";  //String literal (SCP)
-		System.out.println(str1);
-		System.out.println(System.identityHashCode(str1));
-
-		String str2 = "Core2Web";  //String literal (SCP)
-		System.out.println(str2);
-		System.out.println(System.identityHashCode(str2));
-
-		String str5 = "Coreweb";  //String literal (SCP)
-                System.out.println(str5);
-                System.out.println(System.identityHashCode(str5));
-
-		String str3 = new String("Core2Web");   //Object type String or new String(Heap)
-		System.out.println(str3);  
-		System.out.println(System.identityHashCode(str3));
-
-		String str4 = new String("Core2Web");  //Object type String or new String(Heap)
-                System.out.println(str4);
-		System.out.println(System.identityHashCode(str4));
+		Scanner sc = new Scanner(System.in);
+		
+		String info = sc.next();
+		System.out.println(info);
 	}
 }
