d34cf54650d9937edde24cc5ad8b414df0167b03
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-15 22:03:22 +0530

    14

--- Assignment/Codes-Java/Codes/p14.java
+++ Assignment/Codes-Java/Codes/p14.java
@@ -1,19 +1,11 @@
-class StringDemo{
+
+import java.util.*;
+
+class InputDemo{
 	public static void main(String[] args){
-		String str1 = "Shashi";
-		System.out.println(str1);
-		System.out.println(System.identityHashCode(str1));
+		Scanner sc = new Scanner(System.in);
 		
-		String str2 = "Shashi"; 
-		System.out.println(str2);
-		System.out.println(System.identityHashCode(str2));
-
-		String str3 = new String("Shashi");
-		System.out.println(str3);
-		System.out.println(System.identityHashCode(str3));
-
-		String str4 = new String("Shashi");   
-		System.out.println(str4);
-		System.out.println(System.identityHashCode(str4));
+		String info = sc.nextLine();
+		System.out.println(info);
 	}
 }
