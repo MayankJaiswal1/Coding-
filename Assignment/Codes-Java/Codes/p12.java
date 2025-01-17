0afbd771899c183e6eed9d7266f6b349b95ddb53
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-17 22:21:57 +0530

    Add files via upload

--- Assignment/Codes-Java/Codes/p12.java
+++ Assignment/Codes-Java/Codes/p12.java
@@ -1,26 +1,6 @@
 
-import java.util.*;
-
-class InputDemo{
+class TwoDArray{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter your name : ");
-		String name = sc.next();
-		
-		System.out.println("Enter society name :  ");
-		String socName = sc.next();
-		//System.out.println("Enter flatno. : ");
-		//int flatNo = Integer.parseInt(br.readLine());
-		System.out.println("Enter wing name : ");
-		char wing = sc.next().charAt(0);
-
-		System.out.println("Enter flatno.");
-		int flatNo = sc.nextInt();
-
-		System.out.println("Enter your name " + name);
-		System.out.println("Enter society name " + socName);
-		System.out.println("Enter wing name " + wing);
-		System.out.println("Enter flatno. " + flatNo);
+		int arr1[][] = new int[0][0];
 	}
 }
