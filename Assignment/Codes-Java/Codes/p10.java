b7f96792e3d25849ad52d623ebfdbf590c2aac15
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-17 22:21:35 +0530

    2

--- Assignment/Codes-Java/Codes/p10.java
+++ Assignment/Codes-Java/Codes/p10.java
@@ -1,22 +1,5 @@
-import java.io.*;
-
-class InputDemo{
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter your name : ");
-		String name = br.readLine();
-		System.out.println("Enter society name :  ");
-		String socName = br.readLine();
-		//System.out.println("Enter flatno. : ");
-		//int flatNo = Integer.parseInt(br.readLine());
-		System.out.println("Enter wing name : ");
-		char wing = (char)br.read();
-		System.out.println("Enter flatno.");
-		String flatNo = br.readLine();
-
-		System.out.println("Enter your name " + name);
-		System.out.println("Enter society name " + socName);
-		System.out.println("Enter wing name " + wing);
-		System.out.println("Enter flatno. " + flatNo);
+class TwoDArray{
+	public static void main(String[] args){
+		int arr1[][] = new int[][];
 	}
 }
