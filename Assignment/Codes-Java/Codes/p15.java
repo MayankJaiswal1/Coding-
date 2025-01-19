073a90c8e058f5780afd437cd46a53e6e0ecf697
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-19 21:17:14 +0530

    T

--- Assignment/Codes-Java/Codes/p15.java
+++ Assignment/Codes-Java/Codes/p15.java
@@ -1,21 +1,5 @@
-
-import java.util.*;
-
-class SingleLine{
+class TwoDArray{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter player info");
-		String info = sc.nextLine();
-
-		StringTokenizer st = new StringTokenizer(info," ");
-		String str1 = st.nextToken();
-		String str2 = st.nextToken();
-		String str3 = st.nextToken();
-		String str4 = st.nextToken();
-		
-		System.out.println("Player name: " + str1);
-		System.out.println("Jersey No.:  " + str2);
-		System.out.println("Average : "    + str3);
-		System.out.println("Grade : "      + str4);
+		int arr1[][] = new int[][3];
 	}
 }
