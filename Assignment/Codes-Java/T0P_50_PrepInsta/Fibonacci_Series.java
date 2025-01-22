98166d116bb091c529ce0cbf68282c6317a29a65
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2025-01-22 17:18:34 +0530

    Fibonacci_Series_No._II

--- Assignment/Codes-Java/T0P_50_PrepInsta/Fibonacci_Series.java
+++ Assignment/Codes-Java/T0P_50_PrepInsta/Fibonacci_Series.java
@@ -7,6 +7,20 @@ Input : 4
 Output : 0 1 1 2
 */
 
+/*Fibonacci Series
+A series of numbers in which each number is the sum of the two preceding numbers is known as a Fibonacci Series.
+
+General Formula to find the Nth term of a Series 
+F0 = 0 and F1 = 1 be the seed values
+The Nth term of a Fibonacci series is given as Fn = Fn-1 + Fn-2 .
+Example, 
+Input : 5
+Fibonacci Series : 0 1 1 2 3
+Explanation:
+For 3rd term it's 2nd term + 1st term.
+For 4th term it's 3rd term + 2nd term.
+for 5th term it's 4th term + 3rd term. */
+
 
 package T0P_50_PrepInsta;
 
