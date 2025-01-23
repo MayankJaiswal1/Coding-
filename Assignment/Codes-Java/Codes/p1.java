ed0b86648e5b420c66d3d811436497c381dd6915
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-23 22:48:44 +0530

    1

--- Assignment/Codes-Java/Codes/p1.java
+++ Assignment/Codes-Java/Codes/p1.java
@@ -1,5 +1,22 @@
-class ArrayDemo{
+import java.util.*;
+
+class TreeMapMethod{
 	public static void main(String[] args){
-		int arr[] = new int[5]{};
+		TreeMap tm = new TreeMap();
+		tm.put(18,"Virat");
+		tm.put(1,"Virat");
+		tm.put(19,"Virat");
+		tm.put(32,"Virat");
+		tm.put(37,"Virat");
+		System.out.println(tm);
+
+
+		System.out.println(tm.firstKey());
+		System.out.println(tm.lastKey());
+		System.out.println(tm.getCeilingEntry(19));
+		System.out.println(tm.getFloorEntry(19));
+		System.out.println(tm.getHigherEntry(19));
+		System.out.println(tm.getLowerEnter(19));
 	}
 }
+
