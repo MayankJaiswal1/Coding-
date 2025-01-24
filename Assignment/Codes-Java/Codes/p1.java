5b3f036b3a2d5960b29daa82fbddf258301b6e23
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-24 21:29:21 +0530

    1

--- Assignment/Codes-Java/Codes/p1.java
+++ Assignment/Codes-Java/Codes/p1.java
@@ -1,22 +1,10 @@
-import java.util.*;
-
-class TreeMapMethod{
+class JVMInternal{
 	public static void main(String[] args){
-		TreeMap tm = new TreeMap();
-		tm.put(18,"Virat");
-		tm.put(1,"Virat");
-		tm.put(19,"Virat");
-		tm.put(32,"Virat");
-		tm.put(37,"Virat");
-		System.out.println(tm);
-
-
-		System.out.println(tm.firstKey());
-		System.out.println(tm.lastKey());
-		System.out.println(tm.getCeilingEntry(19));
-		System.out.println(tm.getFloorEntry(19));
-		System.out.println(tm.getHigherEntry(19));
-		System.out.println(tm.getLowerEnter(19));
+		int x = 10; 
+		int y = 10;
+		int z = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
 	}
 }
-
