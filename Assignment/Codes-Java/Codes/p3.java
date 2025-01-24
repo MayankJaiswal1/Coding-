064454432f31b75b3f74a3bf0ae38df1474f57d4
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-24 21:29:41 +0530

    3

--- Assignment/Codes-Java/Codes/p3.java
+++ Assignment/Codes-Java/Codes/p3.java
@@ -1,35 +1,15 @@
-import java.util.HashSet;
 
-class HashSetDemo {
-    public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>();
-
-        // Adding elements to the HashSet
-        set.add("Cpp");
-        set.add("React");
-        set.add("Flutter");
-        set.add("SpringBoot");
-        set.add("Java");
-        set.add("Python");
-        set.add("Swift");
-
-        // Printing the HashSet
-        System.out.println(set);
-
-        // Calculating hash codes and bucket indices for educational purposes
-        printHashDetails("Cpp");
-        printHashDetails("React");
-        printHashDetails("Flutter");
-        printHashDetails("SpringBoot");
-        printHashDetails("Java");
-        printHashDetails("Python");
-        printHashDetails("Swift");
-    }
-
-    public static void printHashDetails(String element) {
-        int hashCode = element.hashCode();
-        int bucketIndex = hashCode % 16; // Assuming table size is 16 for simplicity
-        System.out.println("Element: " + element + ", HashCode: " + hashCode + ", BucketIndex: " + bucketIndex);
-    }
+class JVMInternal{
+	public static void main(String[] args){
+		int x = 100; 
+		int y = 110;
+		int z = 100;
+		int a = 128;
+		int b = 128;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(b));
+	}
 }
-
