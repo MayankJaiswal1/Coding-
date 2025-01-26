c54c5d5287e175dd9c096783ba21895542d2c04d
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-26 21:13:20 +0530

    7

--- Assignment/Codes-Java/Codes/p7.java
+++ Assignment/Codes-Java/Codes/p7.java
@@ -1,17 +1,9 @@
-import java.util.*;
 
-class ArrayDemo{
+class Demo{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter array size: ");
-		int size = sc.nextInt();
-		int arr[] = new int[size];
-		//System.out.println(" Size of an array: "+arr.length);
-		for(int i=0; i<arr.length; i++){
-			arr[i]=sc.nextInt();
-		}
-		for(int i=0; i<arr.length; i++){
-			System.out.println(arr[i]);
-		}
+		int x = 10;
+		int y = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
 	}
 }
