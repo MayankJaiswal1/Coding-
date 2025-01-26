2730bbe3be4406e3d49ee913ad96806921a4c044
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-26 21:13:30 +0530

    8

--- Assignment/Codes-Java/Codes/p8.java
+++ Assignment/Codes-Java/Codes/p8.java
@@ -1,19 +1,12 @@
-import java.util.*;
-
-class ArrayDemo{
+class Demo{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter array size: ");
-		int size = sc.nextInt();
-		int arr[] = new int[size];
-		//System.out.println(" Size of an array: "+arr.length);
-		for(int i=0; i<arr.length; i++){
-			System.out.print("Enter elements: ");
-			arr[i]=sc.nextInt();
-		}
-		System.out.println("Array Elements are: ");
-		for(int i=0; i<arr.length; i++){
-			System.out.println(arr[i]);
-		}
+		int x = 10;
+		int y = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+
+		y = 20;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
 	}
 }
