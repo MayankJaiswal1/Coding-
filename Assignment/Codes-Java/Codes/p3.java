ab90787b96df96915f8c4057541f2ea2b573c8be
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-01-29 22:06:27 +0530

    10

--- Assignment/Codes-Java/Codes/p3.java
+++ Assignment/Codes-Java/Codes/p3.java
@@ -1,15 +1,6 @@
 
-class JVMInternal{
+class ArrayDemo{
 	public static void main(String[] args){
-		int x = 100; 
-		int y = 110;
-		int z = 100;
-		int a = 128;
-		int b = 128;
-		System.out.println(System.identityHashCode(x));
-		System.out.println(System.identityHashCode(y));
-		System.out.println(System.identityHashCode(z));
-		System.out.println(System.identityHashCode(a));
-		System.out.println(System.identityHashCode(b));
+		int arr[] = new int[0];
 	}
 }
