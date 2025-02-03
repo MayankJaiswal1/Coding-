d3ed0832dae3798701786b9cc04717c1d930d0ae
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-03 21:29:24 +0530

    4

--- Assignment/Codes-Java/Codes/p4.java
+++ Assignment/Codes-Java/Codes/p4.java
@@ -1,6 +1,17 @@
-class ArrayDemo{
+
+class JVMInternal{
 	public static void main(String[] args){
-		int arr[] = new int[]{};
-		System.out.println(arr[0]);
+		char ch1 = 'A';  //65
+		char ch2 = 'a';  //97  
+		char ch3 = 'B';  //66
+		int x = 65;
+		int y = 97;
+		int z = 66;
+		System.out.println(System.identityHashCode(ch1));
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(ch2));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(ch3));
+		System.out.println(System.identityHashCode(z));
 	}
 }
