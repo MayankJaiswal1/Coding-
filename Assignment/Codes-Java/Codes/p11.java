8e59a25d0d88694c11ded8359d854a5aa542f5c1
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-12 21:17:59 +0530

    3

--- Assignment/Codes-Java/Codes/p11.java
+++ Assignment/Codes-Java/Codes/p11.java
@@ -1,6 +1,25 @@
 
-class TwoDArray{
-	public static void main(String[] args){
-		int arr1[][] = new int[3][0];
+import java.io.*;
+
+class InputDemo{
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your name : ");
+		String name = br.readLine();
+		System.out.println("Enter society name :  ");
+		String socName = br.readLine();
+		//System.out.println("Enter flatno. : ");
+		//int flatNo = Integer.parseInt(br.readLine());
+		System.out.println("Enter wing name : ");
+		char wing = (char)br.read();
+		br.skip(1);
+
+		System.out.println("Enter flatno.");
+		String flatNo = br.readLine();
+
+		System.out.println("Enter your name " + name);
+		System.out.println("Enter society name " + socName);
+		System.out.println("Enter wing name " + wing);
+		System.out.println("Enter flatno. " + flatNo);
 	}
 }
