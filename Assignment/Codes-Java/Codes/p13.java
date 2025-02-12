47d6f65af639dd7b4117a83e2bcf09b8b4320265
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-12 21:18:16 +0530

    5

--- Assignment/Codes-Java/Codes/p13.java
+++ Assignment/Codes-Java/Codes/p13.java
@@ -1,11 +1,11 @@
 
-class TwoDArray{
+import java.util.*;
+
+class InputDemo{
 	public static void main(String[] args){
-		int arr1[][] = new int[2][2];
-		arr1[0][0] = 10;
-		arr1[0][1] = 20;
-		arr1[1][0] = 30;
-		arr1[1][1] = 40;
-		System.out.println(arr1[0][0]);
+		Scanner sc = new Scanner(System.in);
+		
+		String info = sc.next();
+		System.out.println(info);
 	}
 }
