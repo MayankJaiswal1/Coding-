fc3d3db49826e3d12828f858032ca2a7f15ac4e9
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-16 22:05:39 +0530

    13

--- Assignment/Codes-Java/Codes/p13.java
+++ Assignment/Codes-Java/Codes/p13.java
@@ -1,11 +1,14 @@
-
-import java.util.*;
-
-class InputDemo{
+class JaggedArray{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		String info = sc.next();
-		System.out.println(info);
+		int arr[][] = new int[2][];
+		arr[0] = new int[3];
+		arr[1] = new int[2];
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println(arr[1][2]);
 	}
 }
