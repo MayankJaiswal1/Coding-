7659e8383bd68166dbbeb69a291ba6bfe07fb77f
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-16 22:05:28 +0530

    12

--- Assignment/Codes-Java/Codes/p12.java
+++ Assignment/Codes-Java/Codes/p12.java
@@ -1,26 +1,14 @@
 
-import java.util.*;
-
-class InputDemo{
+class JaggedArray{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter your name : ");
-		String name = sc.next();
-		
-		System.out.println("Enter society name :  ");
-		String socName = sc.next();
-		//System.out.println("Enter flatno. : ");
-		//int flatNo = Integer.parseInt(br.readLine());
-		System.out.println("Enter wing name : ");
-		char wing = sc.next().charAt(0);
-
-		System.out.println("Enter flatno.");
-		int flatNo = sc.nextInt();
-
-		System.out.println("Enter your name " + name);
-		System.out.println("Enter society name " + socName);
-		System.out.println("Enter wing name " + wing);
-		System.out.println("Enter flatno. " + flatNo);
+		int arr[][] = new int[2][];
+		arr[0] = new int[3];
+		arr[1] = new int[2];
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
