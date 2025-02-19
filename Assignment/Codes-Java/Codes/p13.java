a91f03e545291d1998fe15e2c6a27889fcceba8d
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-19 21:00:36 +0530

    S13

--- Assignment/Codes-Java/Codes/p13.java
+++ Assignment/Codes-Java/Codes/p13.java
@@ -1,14 +1,23 @@
-class JaggedArray{
+class StringDemo{
 	public static void main(String[] args){
-		int arr[][] = new int[2][];
-		arr[0] = new int[3];
-		arr[1] = new int[2];
-		for(int i=0; i<arr.length; i++){
-			for(int j=0; j<arr[i].length; j++){
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println(arr[1][2]);
+		String str1 = "Core2Web";  //String literal (SCP)
+		System.out.println(str1);
+		System.out.println(System.identityHashCode(str1));
+
+		String str2 = "Core2Web";  //String literal (SCP)
+		System.out.println(str2);
+		System.out.println(System.identityHashCode(str2));
+
+		String str5 = "Coreweb";  //String literal (SCP)
+                System.out.println(str5);
+                System.out.println(System.identityHashCode(str5));
+
+		String str3 = new String("Core2Web");   //Object type String or new String(Heap)
+		System.out.println(str3);  
+		System.out.println(System.identityHashCode(str3));
+
+		String str4 = new String("Core2Web");  //Object type String or new String(Heap)
+                System.out.println(str4);
+		System.out.println(System.identityHashCode(str4));
 	}
 }
