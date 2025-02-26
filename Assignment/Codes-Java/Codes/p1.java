0a64340f8e4d4e2c1fa5d1ba08f37ee75d30eed0
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-26 22:04:25 +0530

    P1

--- Assignment/Codes-Java/Codes/p1.java
+++ Assignment/Codes-Java/Codes/p1.java
@@ -1,18 +1,10 @@
-import java.util.*;
-
-class OneInput{
+class JVMInternal{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter array size");
-		int arrSize = sc.nextInt();
-
-		int arr[] = new int[arrSize];
-		System.out.println("Enter Elements: " );
-		for(int i=0; i<arr.length; i++){
-			arr[i] = sc.nextInt();
-		}
-		for(int i=0; i<arr.length; i++){
-			System.out.println(arr[i]);
-		}
+		int x = 10; 
+		int y = 10;
+		int z = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
 	}
 }
