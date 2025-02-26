57c835ddc89d1ec83aba814a3b5d913593f08ab3
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-26 22:04:41 +0530

    P2

--- Assignment/Codes-Java/Codes/p2.java
+++ Assignment/Codes-Java/Codes/p2.java
@@ -1,18 +1,15 @@
-import java.io.*;
+class JVMInternal{
+	public static void main(String[] args){
+		int x = 10; 
+		int y = 10;
+		int z = 10;
+		int a = 10;
+		int b = 20;
 
-class OneInput{
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter array size");
-		int arrSize = Integer.parseInt(br.readLine());
-
-		int arr[] = new int[arrSize];
-		System.out.println("Enter Elements: " );
-		for(int i=0; i<arr.length; i++){
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		for(int i=0; i<arr.length; i++){
-			System.out.println(arr[i]);
-		}
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(b));
 	}
 }
