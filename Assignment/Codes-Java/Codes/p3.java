dbcf082c857802e92283821603acbab4e024bce0
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-26 22:04:54 +0530

    P3

--- Assignment/Codes-Java/Codes/p3.java
+++ Assignment/Codes-Java/Codes/p3.java
@@ -1,25 +1,15 @@
-import java.util.*;
 
-class OneInput{
+class JVMInternal{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter size for rows and columns : ");
-		int rows = sc.nextInt();
-		int cols = sc.nextInt();
-
-		int arr[][] = new int[rows][cols];
-		System.out.println("Enter array Elements: " );
-		for(int i=0; i<arr.length; i++){
-			for( int j=0; j<arr.length; j++){
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		System.out.println(" Array Elements are: ");
-		for(int i=0; i<arr.length; i++){
-                        for( int j=0; j<arr.length; j++){
-                                System.out.print(arr[i][j] + " ");
-                        }
-			System.out.println();
-                } 
+		int x = 100; 
+		int y = 110;
+		int z = 100;
+		int a = 128;
+		int b = 128;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(b));
 	}
 }
