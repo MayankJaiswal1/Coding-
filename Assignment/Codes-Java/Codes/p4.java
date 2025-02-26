c0ceaf34337ca1b8e035402958f088d2c1cb3900
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-26 22:05:08 +0530

    P4

--- Assignment/Codes-Java/Codes/p4.java
+++ Assignment/Codes-Java/Codes/p4.java
@@ -1,28 +1,17 @@
-import java.util.*;
 
-class TwoDArray{
+class JVMInternal{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter size for rows and columns : ");
-		int rows = sc.nextInt();
-		int cols = sc.nextInt();
-
-		int arr[][] = new int[rows][cols];
-		System.out.println("Enter array Elements: " );
-		for(int i=0; i<arr.length; i++){
-			for( int j=0; j<arr.length; j++){
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		System.out.println(" Array Elements are: ");
-		for(int i=0; i<arr.length; i++){
-                        for( int j=0; j<arr.length; j++){
-                                System.out.print(arr[i][j] + " ");
-                        }
-			System.out.println();
-                }
-	       	System.out.println(arr[1][1]);  //50
-		System.out.println(arr[1]);     //[I@... 
-		System.out.println(arr);        //[[I@...  
+		char ch1 = 'A';  //65
+		char ch2 = 'a';  //97  
+		char ch3 = 'B';  //66
+		int x = 65;
+		int y = 97;
+		int z = 66;
+		System.out.println(System.identityHashCode(ch1));
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(ch2));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(ch3));
+		System.out.println(System.identityHashCode(z));
 	}
 }
