44075191385a65ed85e2130b291a5fbdf0a846e7
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-27 22:03:48 +0530

    P7

--- Assignment/Codes-Java/Codes/p7.java
+++ Assignment/Codes-Java/Codes/p7.java
@@ -1,9 +1,9 @@
-class ThreeDArray{
+
+class Demo{
 	public static void main(String[] args){
-		float arr[][][] = new float[2][3][3];
-		System.out.println(arr[1][1][1]); //0
-		System.out.println(arr[1][1]);    //[F@
-		System.out.println(arr[1]);       //[[F@
-		System.out.println(arr);          //[[[F@
+		int x = 10;
+		int y = 10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
 	}
 }
