8352795b5e7db4448f2ee4c9a763151c052689ac
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-02-28 20:29:27 +0530

    Aa

--- Assignment/Codes-Java/Codes/p13.java
+++ Assignment/Codes-Java/Codes/p13.java
@@ -1,23 +1,11 @@
-class StringDemo{
+
+class TwoDArray{
 	public static void main(String[] args){
-		String str1 = "Core2Web";  //String literal (SCP)
-		System.out.println(str1);
-		System.out.println(System.identityHashCode(str1));
-
-		String str2 = "Core2Web";  //String literal (SCP)
-		System.out.println(str2);
-		System.out.println(System.identityHashCode(str2));
-
-		String str5 = "Coreweb";  //String literal (SCP)
-                System.out.println(str5);
-                System.out.println(System.identityHashCode(str5));
-
-		String str3 = new String("Core2Web");   //Object type String or new String(Heap)
-		System.out.println(str3);  
-		System.out.println(System.identityHashCode(str3));
-
-		String str4 = new String("Core2Web");  //Object type String or new String(Heap)
-                System.out.println(str4);
-		System.out.println(System.identityHashCode(str4));
+		int arr1[][] = new int[2][2];
+		arr1[0][0] = 10;
+		arr1[0][1] = 20;
+		arr1[1][0] = 30;
+		arr1[1][1] = 40;
+		System.out.println(arr1[0][0]);
 	}
 }
