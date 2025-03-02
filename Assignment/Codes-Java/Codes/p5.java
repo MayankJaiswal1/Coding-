ef6e191ace3a9742143dd95c147e9af8a8bc4bcb
MayankJaiswal1 <160144640+MayankJaiswal1@users.noreply.github.com>
2025-03-02 21:00:19 +0530

    5

--- Assignment/Codes-Java/Codes/p5.java
+++ Assignment/Codes-Java/Codes/p5.java
@@ -1,43 +1,7 @@
-class JVMInternal{
+class ArrayDemo{
 	public static void main(String[] args){
-		char ch1 = 'A';  //65
-		char ch2 = 'a';  //97  
-		char ch3 = 'B';  //66
-		int x = 65;
-		//int y = 97;
-		//int z = 66;
-		/*System.out.println(System.identityHashCode(ch1));
-		System.out.println(System.identityHashCode(x));
-		System.out.println(System.identityHashCode(ch2));
-		System.out.println(System.identityHashCode(y));
-		System.out.println(System.identityHashCode(ch3));
-		System.out.println(System.identityHashCode(z));*/
+		int arr[] = new int[]{};
+		arr[0] = 10; //assign the data
+		System.out.println(arr[0]); //access the data
 	}
 }
-/*
-Byte code of the program: 
-   
-mac@LAPTOP-C6S5TUPQ:~/java/Dailycodes/Arrays/Interger_Cache 26_12-March$ vim p5.java
-mac@LAPTOP-C6S5TUPQ:~/java/Dailycodes/Arrays/Interger_Cache 26_12-March$ javac p5.java
-mac@LAPTOP-C6S5TUPQ:~/java/Dailycodes/Arrays/Interger_Cache 26_12-March$ javap -c JVMInternal.class
-Compiled from "p5.java"
-class JVMInternal {
-  JVMInternal();
-    Code:
-       0: aload_0
-       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
-       4: return
-
-  public static void main(java.lang.String[]);
-    Code:
-       0: bipush        65
-       2: istore_1
-       3: bipush        97
-       5: istore_2
-       6: bipush        66
-       8: istore_3
-       9: bipush        65
-      11: istore        4
-      13: return
-}
-*/
